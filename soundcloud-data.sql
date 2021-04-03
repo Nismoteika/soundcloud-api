6848d8a1a746612880f4b90215129d53b699f511
Nismoteika <stepanpahska46@gmail.com>
2021-04-03 21:39:30 +0300

    - db structure for api

---
+++ soundcloud-data.sql
@@ -0,0 +1,61 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `soundcloud-data`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `media_artists`
+--
+
+CREATE TABLE `media_artists` (
+  `id` int NOT NULL,
+  `nick_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `full_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `city` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `description` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `media_tracks`
+--
+
+CREATE TABLE `media_tracks` (
+  `id` int NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `published` datetime NOT NULL,
+  `genre` varchar(255) NOT NULL,
+  `artist_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `media_artists`
+--
+ALTER TABLE `media_artists`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `media_tracks`
+--
+ALTER TABLE `media_tracks`
+  ADD PRIMARY KEY (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
